7238570e1bd0f975c1d69b9b9ecdf73190934491
Matthew Sackman <matthew@rabbitmq.com>
2011-04-03 12:26:02 +0100

    Refine test to demonstrate record field order in record declaration is not relevant; only order in record init

--- test/src/test_cut.erl
+++ test/src/test_cut.erl
@@ -46,4 +46,4 @@ test_cut_tuple() ->
 test_cut_record() ->
     true = #r{} =/= #r{f3 = _},
     orange = ((#r{f3 = _})(orange))#r.f3,
-    {r, foo, bar, baz} = (#r{f1 = _, f2 = _, f3 = _})(foo, bar, baz).
+    {r, foo, bar, baz} = (#r{f1 = _, f3 = _, f2 = _})(foo, baz, bar).
