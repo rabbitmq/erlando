3d457fe0a8f0a4ee734ec64d4bc1e7dd0e3754bd
Matthew Sackman <matthew@rabbitmq.com>
2011-06-07 12:46:06 +0100

    Remove -import_as attributes as we parse them: this makes the transformer idempotent. Also report errors in the structure of the attribute helpfully

--- src/import_as.erl
+++ src/import_as.erl
@@ -28,26 +28,30 @@ parse_transform(Forms, _Options) ->
 
 import_as({attribute, Line, import_as, {Module, Aliases}}, Acc) ->
     import_as({attribute, Line, import_as, [{Module, Aliases}]}, Acc);
-import_as({attribute, _Line, import_as, List} = Attribute, {Funs, Acc})
+import_as({attribute, Line, import_as, List}, {Funs, Acc})
   when is_list(List) ->
-    Funs1 = lists:foldl(
-              fun ({Module, Aliases}, Acc1) ->
-                      [alias_fun(Module, Alias) || Alias <- Aliases] ++ Acc1
-              end, Funs, List),
-    {Funs1, [Attribute | Acc]};
+    Funs1 =
+        lists:foldl(
+          fun ({Module, Aliases}, Acc1) ->
+                  [alias_fun(Module, Line, Alias) || Alias <- Aliases] ++ Acc1
+          end, Funs, List),
+    {Funs1, Acc};
+import_as({attribute, Line, import_as, Wrong}, {Funs, Acc}) ->
+    erlang:error({Line, ?MODULE,
+                  ["Expected a list of pairs or pair, not: ", Wrong]});
 import_as({eof, Line}, {Funs, Acc}) ->
     {Acc1, Line1} =
         lists:foldl(
           fun (Fun, {AccN, LineN}) -> {[Fun(LineN) | AccN], LineN + 1} end,
           {Acc, Line}, Funs),
     {[], [{eof, Line1} | Acc1]};
-import_as(Attribute, {Funs, Acc}) ->
-    {Funs, [Attribute | Acc]}.
+import_as(Other, {Funs, Acc}) ->
+    {Funs, [Other | Acc]}.
 
-alias_fun(Module, {{Dest, Arity}, Alias}) when is_atom(Module) andalso
-                                               is_atom(Dest) andalso
-                                               is_atom(Alias) andalso
-                                               is_integer(Arity) ->
+alias_fun(Module, _AttLine, {{Dest, Arity}, Alias}) when is_atom(Module) andalso
+                                                         is_atom(Dest) andalso
+                                                         is_atom(Alias) andalso
+                                                         is_integer(Arity) ->
     fun (Line) ->
             Vars = [{var, Line, list_to_atom("Var_" ++ integer_to_list(N))} ||
                        N <- lists:seq(1, Arity)],
@@ -55,4 +59,11 @@ alias_fun(Module, {{Dest, Arity}, Alias}) when is_atom(Module) andalso
                     {remote, Line, {atom, Line, Module}, {atom, Line, Dest}},
                     Vars},
             {function, Line, Alias, Arity, [{clause, Line, Vars, [], [Body]}]}
+    end;
+alias_fun(Module, AttLine, WrongThing) ->
+    fun (_Line) ->
+            Str = lists:flatten(io_lib:format("~p", [WrongThing])),
+            erlang:error(
+              {AttLine, ?MODULE,
+               ["Expected a pair of {target_fun/arity, alias}, not: ", Str]})
     end.
